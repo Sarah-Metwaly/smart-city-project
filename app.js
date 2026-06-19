3333a3264bdb618aa99b986622a90bc402cfb798
fatma gamal <fgmal019@gmail.com>
2026-06-19 18:16:54 +0300

    fix(auth): configure CORS origin and enable credentials sharing

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ const hardDeleteRoutes = require('./modules/userAuth/hardDelete/hardDeleteRoutes
 const app = express();
 
 app.use(cookieParser())
-app.use(cors()); //To allow access from the frontEnd Local host
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+// app.use(cors()); //To allow access from the frontEnd Local host
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
