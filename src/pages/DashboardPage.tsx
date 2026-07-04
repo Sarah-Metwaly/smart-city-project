695a58f22d2a18296a5da09a0b30b97c54bda5f7
fatma gamal <fgmal019@gmail.com>
2026-07-04 10:56:39 +0300

    fix: add missing top padding in Home and Police pages to prevent navbar overlap

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -3,7 +3,7 @@ import CityModel from "../components/CityModel/CityModel.tsx";
 
 const HomePage = () => {
   return (
-      <div className="w-full pl-23 pr-23 flex flex-col gap-10 h-screen">
+      <div className="flex flex-col w-full h-screen gap-10 pt-20 pl-23 pr-23">
           <h1 className="font-bold text-[42px] leading-tight tracking-tight">City Twin</h1>
           <p className="text-aman-white text-[15px] font-inter">
               Explore the digital twin of our city, where real-time data meets immersive 3D visualization. Experience the future of urban planning and management with our interactive city model.
